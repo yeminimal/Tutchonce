365dae422e82ce2bcbb2f84445cb33dc0ca6da46
Williams Martins <gh0stphas3@proton.me>
2025-04-27 08:22:22 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -20,5 +19,6 @@ export default defineConfig(({ mode }) => ({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json'], // Added support for .mjs files
   },
 }));
